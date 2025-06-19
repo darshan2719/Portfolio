7ecfb351b7e645038b5921382547097f1f7478a4
darshan.2719 <darshanpatil2719@gmail.com>
2025-06-19 13:57:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings :["Web Developer", "Coder", "Lifetime Student", "CS Undergrad"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
